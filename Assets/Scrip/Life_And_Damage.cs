a19e4b198e7974e8dc5d01f58ba9ad8a907f820f
Mattb26 <81666147+Mattb26@users.noreply.github.com>
2021-05-11 20:24:34 -0300

    Instalacion de Menu
    Agregue una nueva Escena Menu para modificar

--- Assets/Scrip/Life_And_Damage.cs
+++ Assets/Scrip/Life_And_Damage.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Life_And_Damage : MonoBehaviour
 {
-    public int vida;
+    public float vida;
+    public Image barravida;
 
+    void Update()
+    {
+        vida = Mathf.Clamp(vida, 0, 10000);
+        barravida.fillAmount = vida/ 100;
+    }
     public void RestarVida(int cantidad)
     {
         vida -= cantidad;

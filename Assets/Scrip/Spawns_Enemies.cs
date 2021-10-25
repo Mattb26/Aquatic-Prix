ec19d8e1948c8281e52f9e315b53ef3b7918bf04
Mattb26 <81666147+Mattb26@users.noreply.github.com>
2021-10-25 19:41:51 -0300

    Creación de un PauseMenu y oleadas
    Se agrego un script de menú de pausa(Sin UI) y se arreglo el problema de el creador de Oleadas

--- Assets/Scrip/Spawns_Enemies.cs
+++ Assets/Scrip/Spawns_Enemies.cs
@@ -10,11 +10,18 @@ public class Spawns_Enemies : MonoBehaviour
     //public Transform spawn_point;
     public GameObject enemigo;
     [Header("Opciones de Oleadas")]
-    public int n_enemigos;
-    public int dead_enemy;
+    public int n_enemigos = 0;
+    public int dead_enemy = 0;
     public int enemigosAGenerar = 3;
     public int oleada = 1;
-    public int tiempoSpawn;
+    public int tiempoSpawn = 1;
+    public GameObject TheNewParent;
+    private Enemy_Life aver;
+
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,32 +29,43 @@ public class Spawns_Enemies : MonoBehaviour
         // Invokerepeating(nombre de la accion, tiempo inicial, cada cuanto se repite
         InvokeRepeating("spawn", 5, tiempoSpawn);
         //GenerarEnemigos(3);
-    }
 
+
+    }
+    void Awake()
+    {
+
+    }
     // Update is called once per frame
     void Update()
     {
         oleadas();
+
+        //  miunion.a = dead_enemy;
+
+
     }
     void spawn()
     {
         //int i = Random.Range(0, 4);
         //Instantiate(enemigo, spawn_point[i].position, transform.rotation);
         //Instantiate(enemigo, spawn_point.position, transform.rotation);
-        
         if (n_enemigos < enemigosAGenerar)
         {
             int i = Random.Range(0, 3);
-            Instantiate(enemigo, spawn_point[i].position, transform.rotation);
+            Instantiate(enemigo, spawn_point[i].position, transform.rotation, transform.parent = TheNewParent.transform);
+            //enemigo.transform.parent = TheNewParent.transform
             //Instantiate(enemigo, spawn_point.position, transform.rotation);
             print("Enemigo Generado");
             n_enemigos = n_enemigos + 1;
         }
 
     }
-    void oleadas()
+
+    public void oleadas()
     {
-        if(dead_enemy == enemigosAGenerar)
+        //         
+        if (dead_enemy == enemigosAGenerar)
         {
             enemigosAGenerar = enemigosAGenerar + 1;
             oleada++;
@@ -55,13 +73,6 @@ public class Spawns_Enemies : MonoBehaviour
             n_enemigos = enemigosAGenerar;
             dead_enemy = 0;
             n_enemigos = 0;
-        }   
-    }
-    void GenerarEnemigos (int enemigosAGenerar)
-    {
-        for (int e = 0; e < enemigosAGenerar; e++)
-        {
-            spawn();
         }
     }
 }

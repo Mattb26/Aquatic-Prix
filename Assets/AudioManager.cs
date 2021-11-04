bb175e596c262f2b6ecab602a5ebb0f81b848ae2
LuisLaneri <81779885+LuisLaneri@users.noreply.github.com>
2021-11-04 03:05:10 -0300

    Controlador de sonido 2
    ya probado y funcionando completamente el controlador de sonido, solo falta agregar los sonidos a sus triggers correspondiendtes

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -7,9 +7,21 @@ public class AudioManager : MonoBehaviour
 {
 
     public Sound[] sounds;
+
+    public static AudioManager instance;
     
     void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
         
         foreach(Sound s in sounds)
         {
@@ -17,12 +29,18 @@ public class AudioManager : MonoBehaviour
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
     }
 
     public void Play(string name)
     {
         Sound s =Array.Find(sounds, sound => sound.name == name);
+        if(s==null)
+        {
+            Debug.LogWarning("El sonido" + name + "no ha sido encontrado");
+            return;
+        }
         s.source.Play();
     }
 

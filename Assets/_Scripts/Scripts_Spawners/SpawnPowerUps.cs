d072dbc4492cf5e39bbbf4ec3cad28c5dfeb9d16
Mattb26 <81666147+Mattb26@users.noreply.github.com>
2021-11-07 17:18:06 -0300

    Modifique el spawn

--- Assets/_Scripts/Scripts_Spawners/SpawnPowerUps.cs
+++ Assets/_Scripts/Scripts_Spawners/SpawnPowerUps.cs
@@ -7,28 +7,53 @@ public class SpawnPowerUps : MonoBehaviour
     // Start is called before the first frame update
 public GameObject[] spawnObjects;
 public Transform[] spawnLocations;
-
+    public int tiempo;
+    public int objetoinstanciados = 0;
+    public int tiemponormal;
     void Start()
     {
-    InvokeRepeating("instaa", 5.0f, 30.0f);
+        tiemponormal = tiempo;
+        InvokeRepeating("instaa", 5.0f, tiempo);
     }
-
- public void instaa() 
+    private void Update()
     {
-for (int i = 0; i< spawnLocations.Length; i++)
-    { 
-      // Instantiate(spawnObjects[Random.Range(0,spawnObjects.Length)], spawnLocations[Random.Range(0,spawnLocations.Length)]);
-      Instantiate(spawnObjects[Random.Range(0,spawnObjects.Length)], spawnLocations[i]);
-    }
-       // if (other.gameObject.tag == "HealthPowerUp" || other.gameObject.tag == "ShieldPowerUp" || other.gameObject.tag == "SpeedPowerUp"|| other.gameObject.tag == "InvisiblePowerUp" || other.gameObject.tag == "ForceFieldPowerUp")
-      // Find.game
-     // if(transform.childCount > 0)
+        if (objetoinstanciados >= 20)
+        {
+            tiempo = 0;
+        }
+        else if (tiempo == 0 && objetoinstanciados <=20)
+        {
+            tiempo = tiemponormal;
+        }
     }
 
-//Vector3 A(mibarco) = actualposition (15,15,0)
-//Vector3 B(MiPowerUp) = actualposition (5,5,5)
+    // public void instaa() 
+    //    {
+    //for (int i = 0; i< spawnLocations.Length; i++)
+    //    { 
+    //      // Instantiate(spawnObjects[Random.Range(0,spawnObjects.Length)], spawnLocations[Random.Range(0,spawnLocations.Length)]);
+    //      Instantiate(spawnObjects[Random.Range(0,spawnObjects.Length)], spawnLocations[i]);
+    //    }
+    //       // if (other.gameObject.tag == "HealthPowerUp" || other.gameObject.tag == "ShieldPowerUp" || other.gameObject.tag == "SpeedPowerUp"|| other.gameObject.tag == "InvisiblePowerUp" || other.gameObject.tag == "ForceFieldPowerUp")
+    //      // Find.game
+    //     // if(transform.childCount > 0)
+    //    }
 
-//Vector3 C = A - B
-   
-    
+    //    //Vector3 A(mibarco) = actualposition (15,15,0)
+    //    //Vector3 B(MiPowerUp) = actualposition (5,5,5)
+
+    //    //Vector3 C = A - B
+    //    //cosas mati
+    public void instaa()
+    {
+        if (tiempo > 0)
+        {
+            for (int i = 0; i < spawnLocations.Length; i++)
+            {
+                Instantiate(spawnObjects[Random.Range(0, spawnObjects.Length)], spawnLocations[i]);
+                objetoinstanciados = objetoinstanciados + 1;
+            }
+        }
+        
+    }
 }
